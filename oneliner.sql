505078e2c5cd384db707a0e907b55308d0dd1a75
Michael Bernd Beier <michaelberndbeier@MBP-von-Michael.fritz.box>
2021-09-16 20:01:52 +0200

    Kein Grund alle Paare vorzuhalten -> umgestellt, sollte schneller, genügsamer und besser sein

--- oneliner.sql
+++ oneliner.sql
@@ -1,21 +1,11 @@
 select 
-randomOffset.offset,
-senderInfo.*,
-receiverInfo.*
+sender.*,
+receiver.*
 
-from (
-select 
-sender.id as senderId,
-receiver.id as receiverId
 from participants as sender
-cross join participants as receiver) as allPairings
 cross join 
 (
 SELECT ABS(RANDOM() % (select count(*)  - 1 from participants)) + 1 as offset
 ) as randomOffset
-inner join participants as senderInfo
-on allPairings.senderId = senderInfo.id
-inner join participants as receiverInfo
-on allPairings.receiverId = receiverInfo.id
-
-where allPairings.receiverId = (allPairings.senderId + randomOffset.offset) % (select count(*) from participants)
+inner join participants as receiver
+on receiver.id = (sender.id + randomOffset.offset) % (select count(*) from participants)
